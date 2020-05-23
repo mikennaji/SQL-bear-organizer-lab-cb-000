7928696a4f47535d8b0a5a5e7c689599bbf310f1
Michael Nnaji <mnnaji3@uwo.ca>
2020-05-23 18:03:36 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,8 @@
-create TABLE bears(id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive INTEGER);
+create TABLE bears
+  (id INTEGER PRIMARY KEY,
+     name TEXT,
+      age INTEGER,
+       gender TEXT,
+        color TEXT,
+         temperament TEXT,
+          alive INTEGER);
