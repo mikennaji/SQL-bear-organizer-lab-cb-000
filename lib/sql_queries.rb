78a29f40e4cb6343e23b7aff289eeee9a9b1dd68
Michael Nnaji <mnnaji3@uwo.ca>
2020-05-23 17:48:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears WHERE MAX(age)"
+  "SELECT  "
 end
 
 def select_youngest_bear_and_returns_name_and_age
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(mood) FROM bears WHERE mood = 'goofy'" 
 end
 
 def selects_bear_that_killed_Tim
